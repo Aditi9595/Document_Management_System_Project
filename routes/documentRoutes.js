d778456a094306f5d9f42344d47882e656f2f354
Aditi9595 <aditi.chouhan0007@gmail.com>
2024-10-19 18:12:35 +0530

    Update documentRoutes.js

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-// Example route for document creation
+
 router.post('/create', (req, res) => {
-    // Your document creation logic here
+    
     res.send('Document created');
 });
 
 router.get('/:id', (req, res) => {
-    // Your logic to get a document by ID here
+    
     res.send('Document data');
 });
 
-module.exports = router;  // Ensure you are exporting the router
+module.exports = router;  
